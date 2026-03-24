6e020489de6325b46148abbf42059570e6533af0
handziurdmytro <handziurdmytro@gmail.com>
2026-03-24 17:07:05 +0200

    feat(frontend-web): add login and receiving a token features

--- frontend-web/src/api/auth.js
+++ frontend-web/src/api/auth.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, {Axios} from 'axios';
 
 const apiClient = axios.create({
     baseURL: '/api',
@@ -7,8 +7,45 @@ const apiClient = axios.create({
     },
 });
 
+apiClient.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+
+        return token;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+)
+
 export const authApi = {
     register: async (email, password) => {
         return await apiClient.post('/register', {email, password});
+    },
+
+    // login: async (email, password) => {
+    //     return await apiClient.post('/login', {email, password});
+    // }
+
+    login: async (email, password) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve({
+                    status: 200,
+                    data: {
+                        token: "fake-jwt-token-"+password,
+                        user: {
+                            id: 1,
+                            email,
+                            name: "Test User"
+                        }
+                    }
+                });
+            }, 500);
+        });
     }
 }
--- frontend-web/src/components/Auth.jsx
+++ frontend-web/src/components/Auth.jsx
@@ -1,8 +1,11 @@
 import {useState} from 'react';
+import {useNavigate} from 'react-router-dom'
 
 import {authApi} from '../api/auth.js'
 
-export const Auth = () => {
+export const Auth = ({onAuthSuccess}) => {
+    const [isLoginMode, setIsLoginMode] = useState(true);
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
@@ -10,21 +13,43 @@ export const Auth = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [isSuccess, setIsSuccess] = useState(false);
 
+    const navigate = useNavigate();
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         setError(null);
+        setIsSuccess(false)
         setIsLoading(true);
 
         try {
-            const response = await authApi.register(email, password);
+            let response;
+            if (isLoginMode) {
+                response = await authApi.login(email, password);
+            } else {
+                response = await authApi.register(email, password);
+            }
 
-            if (response.status === 200 || response.status === 201){
-                console.log(response.data.email)
-                console.log(response.data.password)
-                setIsSuccess(true);
-                setEmail('');
-                setPassword('');
+            if (response.status === 200 || response.status === 201) {
+
+                if (isLoginMode) {
+                    const token = response.data.token;
+
+                    if (token) {
+                        localStorage.setItem('token', token);
+                        navigate('/dashboard');
+                    } else {
+                        setError('Server did not return a token')
+                    }
+                } else {
+                    /*TO DEL*/
+                    console.log(response.data.email)
+                    console.log(response.data.password)
+                    /**/
+                    setIsSuccess(true);
+                    setPassword('');
+                    setIsLoginMode(true);
+                }
             }
         } catch (err) {
             if (err.response) {
@@ -37,12 +62,18 @@ export const Auth = () => {
         }
     };
 
+    const toggleMode = () => {
+        setIsLoginMode(!isLoginMode);
+        setError(null);
+        setIsSuccess(false);
+    };
+
     return (
         <div className="authContainer">
-            <h2>Registration</h2>
+            <h2>{isLoginMode ? 'Login' : 'Registration'}</h2>
 
-            {error && <div style={{color: 'red'}}>{error}</div>}
-            {isSuccess && <div style={{color: 'green'}}>Success!</div>}
+            {error && <div style={{color: 'red', marginBottom: '10px'}}>{error}</div>}
+            {isSuccess && <div style={{color: 'green', marginBottom: '10px'}}>Success!</div>}
 
             <form onSubmit={handleSubmit}>
                 <div>
@@ -71,9 +102,22 @@ export const Auth = () => {
                 </div>
 
                 <button type="submit" disabled={isLoading}>
-                    {isLoading ? 'Loading...' : 'Register'}
+                    {isLoading ? 'Loading...' : (isLoginMode ? 'Login' : 'Register')}
                 </button>
             </form>
+
+            <div style={{marginTop: '15px'}}>
+                <button
+                    type={"button"}
+                    onClick={toggleMode}
+                    style={{background: 'transparent', border: 'none', color: '#646cff', textDecoration: 'underline'}}
+                >
+                    {
+                        isLoginMode ? "Don't have an account yet? Sign up"
+                            : "Have an account? Log in"
+                    }
+                </button>
+            </div>
         </div>
     );
 }
