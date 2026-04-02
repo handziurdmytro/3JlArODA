1dbee6add95523ca7a83a80ef2e07ffafff1983e
handziurdmytro <handziurdmytro@gmail.com>
2026-04-02 18:46:29 +0300

    feat(business): add sql queries #1-2 for Manager

---
+++ go-service/internal/employee/queries.sql
@@ -0,0 +1,28 @@
+-- name: create-employee
+-- task: Додавати нові дані про працівників (Manager #1)
+INSERT INTO employees (
+    id_employee, empl_surname, empl_name, empl_patronymic, 
+    empl_role, salary, date_of_birth, date_of_start, 
+    phone_number, city, street, zip_code
+) VALUES (
+    $1, $2, $3, $4, 
+    $5, $6, $7, $8, 
+    $9, $10, $11, $12
+);
+
+-- name: update-employee
+-- task: Редагувати дані про працівників (Manager #2)
+UPDATE employees 
+SET 
+    empl_surname = $2, 
+    empl_name = $3, 
+    empl_patronymic = $4, 
+    empl_role = $5, 
+    salary = $6, 
+    date_of_birth = $7, 
+    date_of_start = $8, 
+    phone_number = $9, 
+    city = $10, 
+    street = $11, 
+    zip_code = $12
+WHERE id_employee = $1;
