f200dff754c207c9087ce10668c1dafb218a18a3
handziurdmytro <handziurdmytro@gmail.com>
2026-04-03 21:33:53 +0300

    build(crypto-service): add auth_secret.proto code generation code to build.rs

--- crypto-service/build.rs
+++ crypto-service/build.rs
@@ -1,7 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=../proto/receipt.proto");
+    println!("cargo:rerun-if-changed=../proto/auth_secret.proto");
 
     tonic_prost_build::compile_protos("../proto/receipt.proto")?;
+    tonic_prost_build::compile_protos("../proto/auth_secret.proto")?;
 
     Ok(())
 }
