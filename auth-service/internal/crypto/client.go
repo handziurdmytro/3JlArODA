8a892685093f52caa7f34ceb665e4b4e57579af1
handziurdmytro <handziurdmytro@gmail.com>
2026-04-07 00:22:50 +0300

    feat(auth-service): add gRPC-client to initiate communication with crypto-service

---
+++ auth-service/internal/crypto/client.go
@@ -0,0 +1,82 @@
+package crypto
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/handziurdmytro/3JlArODA/auth-service/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Client struct {
+	grpcClient pb.AuthSecretServiceClient
+	conn       *grpc.ClientConn
+}
+
+func NewClient(address string) *Client {
+	conn, err := grpc.NewClient(
+		address,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+
+	if err != nil {
+		log.Fatalf("[FATAL] failed to connect to crypto-service at %s: %v", address, err)
+	}
+
+	c := &Client{
+		grpcClient: pb.NewAuthSecretServiceClient(conn),
+		conn:       conn,
+	}
+
+	c.ping()
+
+	return c
+}
+
+func (c *Client) ping() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err := c.grpcClient.HashPassword(ctx, &pb.HashRequest{PlainPassword: "ping"})
+	if err != nil {
+		log.Fatalf("[FATAL] crypto-service is unreachable: %v", err)
+	}
+
+	log.Println("[INFO] connected to crypto-service")
+}
+
+func (c *Client) HashPassword(plainPassword string) (string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	resp, err := c.grpcClient.HashPassword(ctx, &pb.HashRequest{PlainPassword: plainPassword})
+	if err != nil {
+		return "", err
+	}
+
+	return resp.HashString, nil
+}
+
+func (c *Client) VerifyPassword(plainPassword, hashString string) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	resp, err := c.grpcClient.VerifyPassword(ctx, &pb.VerifyRequest{
+		PlainPassword: plainPassword,
+		HashString:    hashString,
+	})
+
+	if err != nil {
+		return false, err
+	}
+
+	return resp.IsValid, nil
+}
+
+func (c *Client) Close() {
+	if err := c.conn.Close(); err != nil {
+		log.Printf("[WARN] error closing crypto-service connection: %v", err)
+	}
+}
