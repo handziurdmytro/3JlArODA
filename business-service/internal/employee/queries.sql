23fb2f47a4f0c82431a7904baa5442d1a52f08ac
handziurdmytro <handziurdmytro@gmail.com>
2026-04-11 20:09:54 +0300

    feat(business-service): add queries Manager #3, 5, 6, 11 and Cashier #15

--- business-service/internal/employee/queries.sql
+++ business-service/internal/employee/queries.sql
@@ -1,28 +1,104 @@
 -- name: create-employee
 -- task: Додавати нові дані про працівників (Manager #1)
-INSERT INTO employees (
-    id_employee, empl_surname, empl_name, empl_patronymic, 
-    empl_role, salary, date_of_birth, date_of_start, 
-    phone_number, city, street, zip_code
-) VALUES (
-    $1, $2, $3, $4, 
-    $5, $6, $7, $8, 
-    $9, $10, $11, $12
-);
+INSERT INTO employees (id_employee, empl_surname, empl_name, empl_patronymic,
+                       empl_role, salary, date_of_birth, date_of_start,
+                       phone_number, city, street, zip_code)
+VALUES ($1, $2, $3, $4,
+        $5, $6, $7, $8,
+        $9, $10, $11, $12);
 
--- name: update-employee
+-- name: update-employee-by-id
 -- task: Редагувати дані про працівників (Manager #2)
-UPDATE employees 
-SET 
-    empl_surname = $2, 
-    empl_name = $3, 
-    empl_patronymic = $4, 
-    empl_role = $5, 
-    salary = $6, 
-    date_of_birth = $7, 
-    date_of_start = $8, 
-    phone_number = $9, 
-    city = $10, 
-    street = $11, 
-    zip_code = $12
+UPDATE employees
+SET empl_surname    = $2,
+    empl_name       = $3,
+    empl_patronymic = $4,
+    empl_role       = $5,
+    salary          = $6,
+    date_of_birth   = $7,
+    date_of_start   = $8,
+    phone_number    = $9,
+    city            = $10,
+    street          = $11,
+    zip_code        = $12
 WHERE id_employee = $1;
+
+-- name: delete-employee-by-id
+-- task: Видаляти дані про працівників (Manager #3)
+-- todo: handle On Delete No Action
+DELETE
+FROM employees
+WHERE id_employee = $1;
+
+-- name: get-all-employees
+-- task: Отримати інформацію про усіх працівників, відсортованих за прізвищем (Manager #5)
+SELECT id_employee,
+       empl_surname,
+       empl_name,
+       empl_patronymic,
+       empl_role,
+       salary,
+       date_of_birth,
+       date_of_start,
+       phone_number,
+       city,
+       street,
+       zip_code
+FROM employees
+ORDER BY empl_surname, empl_name, empl_patronymic;
+
+-- name: get-employee-by-id
+-- task: Можливість отримати усю інформацію про себе. (Cashier #15)
+SELECT id_employee,
+       empl_surname,
+       empl_name,
+       empl_patronymic,
+       empl_role,
+       salary,
+       date_of_birth,
+       date_of_start,
+       phone_number,
+       city,
+       street,
+       zip_code
+FROM employees
+WHERE id_employee = $1;
+
+-- name: get-employees-by-role
+-- task: Отримати інформацію про усіх працівників, що займають посаду касира,
+-- відсортованих за прізвищем (Manager #6)
+SELECT id_employee,
+       empl_surname,
+       empl_name,
+       empl_patronymic,
+       empl_role,
+       salary,
+       date_of_birth,
+       date_of_start,
+       phone_number,
+       city,
+       street,
+       zip_code
+FROM employees
+WHERE empl_role = $1
+ORDER BY empl_surname, empl_name, empl_patronymic;
+
+-- name: get-employee-data-by-surname
+-- task: За прізвищем працівника знайти його телефон та адресу (Manager #11)
+SELECT phone_number,
+       city,
+       street,
+       zip_code
+FROM employees
+WHERE empl_surname = $1;
+
+-- name: get-employee-data-by-full-name
+-- task: За прізвищем працівника знайти його телефон та адресу (Manager #11)
+SELECT phone_number,
+       city,
+       street,
+       zip_code
+FROM employees
+WHERE empl_surname = $1
+  AND empl_name = $2
+  AND empl_patronymic = $3;
