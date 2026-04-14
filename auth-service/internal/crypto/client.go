2c5eedde6a7bb6ef04eb7f4d2d81b43755f18b6d
handziurdmytro <handziurdmytro@gmail.com>
2026-04-14 19:24:27 +0300

    feat(auth-service): add signing and validating JWT functionality

--- auth-service/internal/crypto/client.go
+++ auth-service/internal/crypto/client.go
@@ -75,6 +75,37 @@ func (c *Client) VerifyPassword(plainPassword, hashString string) (bool, error) 
 	return resp.IsValid, nil
 }
 
+func (c *Client) SignJWT(userID, username string) (string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	resp, err := c.grpcClient.SignJWT(ctx, &pb.SignJWTRequest{
+		UserId:   userID,
+		Username: username,
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return resp.Token, nil
+}
+
+func (c *Client) ValidateJWT(token string) (*pb.ValidateJWTResponse, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	resp, err := c.grpcClient.ValidateJWT(ctx, &pb.ValidateJWTRequest{
+		Token: token,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
 func (c *Client) Close() {
 	if err := c.conn.Close(); err != nil {
 		log.Printf("[WARN] error closing crypto-service connection: %v", err)
